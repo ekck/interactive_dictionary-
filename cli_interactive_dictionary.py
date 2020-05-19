e1688d65f529dfd8a11d8640cda8b6cc9a39f783
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-05-19 22:26:34 +0300

    near perfect

--- cli_interactive_dictionary.py
+++ cli_interactive_dictionary.py
@@ -7,6 +7,10 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
